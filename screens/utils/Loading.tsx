24e4147214e33a3cc7eba78a2df418a0736782cf
leqx <eugenealex123@gmail.com>
2022-02-09 15:01:12 +0300

    Fixed Icon

--- screens/utils/Loading.tsx
+++ screens/utils/Loading.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { View, ActivityIndicator,Image, Dimensions} from "react-native";
-import { Layout, themeColor } from "react-native-rapi-ui";
+import React from 'react';
+import { View, ActivityIndicator, Image, Dimensions } from 'react-native';
+import { Layout, themeColor } from 'react-native-rapi-ui';
 
-const { width ,height} = Dimensions.get('screen');
+const { width, height } = Dimensions.get('screen');
 
 export default function () {
   return (
@@ -10,11 +10,13 @@ export default function () {
       <View
         style={{
           flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <ActivityIndicator size="large" color={themeColor.primary} />
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Image
+          source={require('../../assets/images/adaptive-icon.png')}
+          style={{ width: 120, height: 120 }}
+        />
       </View>
     </Layout>
   );
